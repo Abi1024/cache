6e89221d93f21334b61cabbab0638aed6b88a3fc
Abiyaz Chowdhury <chowdh2@cooper.edu>
2019-10-12 23:15:13 -0400

    added a test program to properly verify combined operation of stxxl and cgroups, also added a README for myself

---
+++ build/CMakeFiles/adaptive.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/adaptive.dir/cache_adaptive.cpp.o"
+  "adaptive.pdb"
+  "adaptive"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/adaptive.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
