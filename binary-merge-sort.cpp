6b16594dd71c6db5cf2271e6a1c4d7e14b4624b1
Arghy Bhattacharya <iamarghya.1@gmail.com>
2020-01-14 13:34:08 -0500

    binary merge sort with memory allocation

--- binary-merge-sort.cpp
+++ binary-merge-sort.cpp
@@ -1,50 +1,44 @@
 /* C++ program for Merge Sort */
 #include<iostream> 
-#include<vector>
 using namespace std;
 // Merges two subarrays of arr[]. 
 // First subarray is arr[l..m] 
 // Second subarray is arr[m+1..r] 
-void merge(int arr[], int l, int m, int r) { 
+void merge(int arr[], int temp_arr[], int l, int m, int r) { 
 	int i, j, k; 
 	int n1 = m - l + 1; 
 	int n2 = r - m; 
 
-	/* create temp arrays */
-	int L[n1], R[n2]; 
-
 	/* Copy data to temp arrays L[] and R[] */
 	for (i = 0; i < n1; i++) 
-		L[i] = arr[l + i]; 
+		temp_arr[i] = arr[l + i]; 
 	for (j = 0; j < n2; j++) 
-		R[j] = arr[m + 1+ j]; 
+		temp_arr[n1 + j] = arr[m + 1+ j]; 
 
 	/* Merge the temp arrays back into arr[l..r]*/
 	i = 0; // Initial index of first subarray 
 	j = 0; // Initial index of second subarray 
 	k = l; // Initial index of merged subarray 
 	while (i < n1 && j < n2) { 
-		if (L[i] <= R[j]) { 
-			arr[k] = L[i]; 
+		if (temp_arr[i] <= temp_arr[n1 + j]) { 
+			arr[k] = temp_arr[i]; 
 			i++; 
 		} 
 		else { 
-			arr[k] = R[j]; 
+			arr[k] = temp_arr[n1 + j]; 
 			j++; 
 		} 
 		k++; 
 	} 
-
 	/* Copy the remaining elements of L[], if there are any */
 	while (i < n1) { 
-		arr[k] = L[i]; 
+		arr[k] = temp_arr[i]; 
 		i++; 
 		k++; 
-	} 
-
+	}
 	/* Copy the remaining elements of R[], if there are any */
 	while (j < n2) { 
-		arr[k] = R[j]; 
+		arr[k] = temp_arr[n1 + j]; 
 		j++; 
 		k++; 
 	} 
@@ -52,27 +46,26 @@ void merge(int arr[], int l, int m, int r) {
 
 /* l is for left index and r is right index of the 
 sub-array of arr to be sorted */
-void mergeSort(int arr[], int l, int r, int b) { 
-	if (l < r) { 
+void mergeSort(int arr[], int l, int r, int temp_arr[], int b) { 
+	if (l < r && r -l > b) {
 		// Same as (l+r)/2, but avoids overflow for large l and h 
 		int m = l+(r-l)/2; 
-		// Sort first and second halves 
-		if (r - l > b) {
-			mergeSort(arr, l, m, b); 
-			mergeSort(arr, m+1, r, b); 
+		mergeSort(arr, l, m, temp_arr, b); 
+		mergeSort(arr, m+1, r, temp_arr, b);
+		merge(arr, temp_arr, l, m, r);
+	}
+	else if (l < r && r - l <= b) {
+		// Same as (l+r)/2, but avoids overflow for large l and h 
+		int m = l+(r-l)/2;
+		for (int i = 0; i < b; i++) {
+			temp_arr[i] = arr[i + l]; 
 		}
-		else {
-			int temp_arr[b];
-			for (int i = 0; i < b; i++) {
-				temp_arr[i] = arr[i + l]; 
-			}
-			sort(temp_arr, temp_arr + b);
-			for (int i = 0; i < 32; i++) {
-				arr[i + l] = temp_arr[i]; 
-			}
+		sort(temp_arr, temp_arr + b);
+		for (int i = 0; i < b; i++) {
+			arr[i + l] = temp_arr[i]; 
 		}
-		merge(arr, l, m, r); 
-	} 
+		merge(arr, temp_arr, l, m, r); 
+	}
 } 
 
 /* UTILITY FUNCTIONS */
@@ -83,22 +76,26 @@ void printArray(int A[], int size) {
 	cout << endl; 
 } 
 
-/* Driver program to test above functions */
-int main() 
-{ 
-	const unsigned long long num_elements = 128;
-	const unsigned long long base_case = 32;
+void rootMergeSort(int num_elements, int base_case) {
 	int arr[num_elements];
+	int temp_arr[num_elements];
 	for (unsigned long long i = 0; i < num_elements; i++) {
 		arr[i] = rand() % 1000;
   	}
-	printf("Given array is \n"); 
+  	printf("Given array is \n"); 
 	printArray(arr, num_elements); 
-
-	mergeSort(arr, 0, num_elements - 1, base_case); 
-
+	mergeSort(arr, 0, num_elements - 1, temp_arr, base_case); 
 	printf("\nSorted array is \n"); 
-	printArray(arr, num_elements); 
+	printArray(arr, num_elements);
+}
+
+/* Driver program to test above functions */
+int main() 
+{ 
+	cout << "say hi" << endl;
+	const unsigned long long num_elements = 128;
+	const unsigned long long base_case = 32;
+	rootMergeSort(num_elements, base_case);
 	return 0; 
 } 
 
